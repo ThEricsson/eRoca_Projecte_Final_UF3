4834c7bbf1696a1512a8d45fe0177aaf1d8e5409
Eric Roca Herrera <56220682+ThEricsson@users.noreply.github.com>
2022-03-03 20:01:16 +0100

    New css and changed the files security and users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,15 +5,26 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Symfony\Component\Security\Core\User\UserInterface;
+
+use App\Repository\UserRepository;
+
+
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 use Symfony\Component\Validator\Constraints as Assert;
+
+
+
 
 /**
  * User
  *
  * @ORM\Table(name="users")
  * @ORM\Entity
+ * @UniqueEntity("email", message="Aquest email ja està utilitzat")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -29,18 +40,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string|null
      *
-     * @ORM\Column(name="role", type="string", length=50, nullable=true, options={"default"="NULL"})
+     * @ORM\Column(name="role", type="string", length=50, nullable=true)
      */
-    private $role = 'NULL';
+    private $role;
 
     /**
      * @var string|null
      *
-     * @ORM\Column(name="name", type="string", length=100, nullable=true, options={"default"="NULL"})
-     * @Assert\NotBlank()
+     * @ORM\Column(name="name", type="string", length=100, nullable=true)
+     * @Assert\NotBlank
      * @Assert\Regex(
-     *      pattern = "/[a-zA-Z ]+/",
-     *      message = "Només es poden introduir lletres"
+     *     pattern     = "/^[a-z ]+$/i",
+     *     htmlPattern = "[a-zA-Z ]+",
+     *     message = "Només pots escriure lletres majúscules,minúscules i espais"
      * )
      */
     private $name;
@@ -48,11 +60,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string|null
      *
-     * @ORM\Column(name="surname", type="string", length=200, nullable=true, options={"default"="NULL"})
-     * @Assert\NotBlank()
+     * @ORM\Column(name="surname", type="string", length=200, nullable=true)
+     * @Assert\NotBlank
      * @Assert\Regex(
-     *      pattern = "/[a-zA-Z ]+/",
-     *      message = "Només es poden introduir lletres"
+     *     pattern     = "/^[a-z ]+$/i",
+     *     htmlPattern = "[a-zA-Z ]+",
+     *     message = "Només pots escriure lletres majúscules,minúscules i espais"
      * )
      */
     private $surname;
@@ -60,9 +73,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string|null
      *
-     * @ORM\Column(name="email", type="string", length=255, nullable=true, options={"default"="NULL"})
-     * @Assert\Email(
-     *     message = "L'email '{{ value }}' no és un email vàlid."
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+    *  @Assert\Email(
+     *     message = "Aquest '{{ value }}' no és un email vàlid"
      * )
      */
     private $email;
@@ -70,56 +83,28 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string|null
      *
-     * @ORM\Column(name="password", type="string", length=255, nullable=true, options={"default"="NULL"})
-     * @Assert\NotBlank(
-     *      message = "La contrasenya es obligatoria"
-     * )
+     * @ORM\Column(name="password", type="string", length=255, nullable=true)
+     * @Assert\NotBlank
+     * 
      */
     private $password;
 
     /**
      * @var \DateTime|null
      *
-     * @ORM\Column(name="created_at", type="datetime", nullable=true, options={"default"="NULL"})
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
      */
-    private $createdAt = 'NULL';
+    private $createdAt;
 
-    /**
-     * 
-     *  @ORM\OneToMany (targetEntity = "App\Entity\Task", mappedBy="user")
+     /**
+     * @var \Tasks
+     *
+     * @ORM\OneToMany(targetEntity="App\Entity\Task", mappedBy="user")
      */
     private $tasks;
 
-    /**
-     * A visual identifier that represents this user.
-     * 
-     * @see UserInterface 
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->email;
-    }
-
-    /**
-     * @deprecated since Symfony 5.3, use getUserIdentifier instead
-     */
-    public function getUsername(): string
-    {
-        return (string) $this->email;
-    }
 
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles() : array
-    {
-        $roles = $this->roles;
-
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
 
     public function getId(): ?int
     {
@@ -174,10 +159,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+
     /**
      * @see PasswordAuthenticatedUserInterface
      */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
@@ -187,25 +173,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->password = $password;
 
         return $this;
-    }
-
-    /**
-     * Return a salt is only needed, if you are not using a modern
-     * hashing algoritm in your security.yalm
-     * 
-     * @see UserInterface
-     */
-    public function  getSalt(): ?string
-    {
-        return null;
-    }
-
-    /**
-     * @see User interface
-     */
-    public function eraseCredentials()
-    {
-        
     }
 
     public function getCreatedAt(): ?\DateTimeInterface
@@ -219,11 +186,83 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-    /**
+
+     /** 
      * @return Collection|Task[]
      */
-    public function getTasks(): Collection
+    public function getTasks()
     {
         return $this->tasks;
     }
+
+    public function __toString() {
+        return $this->name;
+    }
+
+     /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        // $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+     /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+        /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    
+
+
+ 
+
+
+
+
+
 }
